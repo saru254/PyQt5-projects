1b095e3b5cf50735460826fdaeb84d8065173bb4
Karen Meni <107751782+saru254@users.noreply.github.com>
2023-09-24 21:28:48 +0300

    Update main.py

--- temp convertor/main.py
+++ temp convertor/main.py
@@ -3,9 +3,23 @@ from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdi
 import math
 
 
-class TempConverter(QMainWindow):
+class TempConverter(QWidget):
   def __init__(self):
     super().__init__()
+    self.init_ui()
+
+
+def init_ui(self):
+  self.setWindowTitle("TEMPERATURE CONVERTER")
+  self.setGeometry(100,100,300,150)
+  self.label_celsius = QLabel("Enter Temp in Celsius:")
+  self.input_celsius =QLineEdit()
+  self.label_fahrenheit = QLabel("Temp in Fahrenheit:")
+  self.result_fahreheit = QLabel("")
+  
+
+
+
 
 
 
