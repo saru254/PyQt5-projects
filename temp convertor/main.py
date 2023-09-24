a4e51e49d4830e6a50c3108be8a8e628fdcef094
Karen Meni <107751782+saru254@users.noreply.github.com>
2023-09-24 21:18:04 +0300

    Update main.py

--- temp convertor/main.py
+++ temp convertor/main.py
@@ -1,3 +1,9 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton
 import math
+
+
+class TempConverter(QMainWindow):
+  def __init__(self):
+    super().__init__()
+
