9f038d2044e3bdcf1d5a627aee3c2223e746c5c8
Karen Meni <107751782+saru254@users.noreply.github.com>
2023-10-02 14:06:51 +0300

    Update main.py

--- period tracker/main.py
+++ period tracker/main.py
@@ -1,3 +1,5 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QCalendarWidget, QVBoxLayout, QWidget, QFormLayout, QComboBox
 from PyQt5.QtCore import Qt, Qdate, QTimer
+from PyQt5.QtGui import QIcon
+
